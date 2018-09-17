6c22776b0676ebb6b81d76e42f4e43d843b875d1
Vadim Denisov <gidsor@gmail.com>
2018-09-18 00:32:56 +0500

    Added Proxy

---
+++ Playgrounds/Proxy.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+protocol VideoServiceProtocol {
+    func loadVideoList(url: URL?) -> [Data]?
+}
+
+class VideoService : VideoServiceProtocol {
+    func loadVideoList(url: URL?) -> [Data]? {
+        // loading from network
+        return []
+    }
+}
+
+class Proxy : VideoServiceProtocol {
+    var service: VideoServiceProtocol?
+    private var cashedData: [Data]?
+    
+    init(service: VideoServiceProtocol) {
+        self.service = service
+    }
+    
+    func loadVideoList(url: URL?) -> [Data]? {
+        if cashedData == nil {
+            cashedData = service?.loadVideoList(url: url)
+        }
+        return cashedData
+    }
+}
+
+var videoService = VideoService()
+var proxy = Proxy(service: videoService)
+proxy.loadVideoList(url: nil)
